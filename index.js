ca9098baac84e14babfef429aba32654ddb374e7
damian <damiankohut@gmail.com>
2021-11-03 15:12:17 +0000

    finish

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+// Declare a function name reverse string that takes string parameter.
+// Initialized  the string
+// Declare for loop 
+// Reverse the string 
+// Print the console.log
+
+function reverseString(str){
+  let newString = "";
+  for(let i = str.length - 1; i >= 0; i--){
+        newString = newString + str[i];
+} 
+return newString 
+}
+console.log(reverseString("damian"))
